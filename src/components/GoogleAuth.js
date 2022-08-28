7f6442728dfe7e339fb2fba3856bb7939d97c80c
jordanss1 <jordan.shade@live.com>
2022-08-28 16:52:31 +0100

    initial commit, setup skeleton and installed redux and other packages

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const GoogleAuth = () => {
+  return (
+    <div>
+      <button class="ui labeled icon button">
+        Login with
+        <i class="google icon"></i>
+      </button>
+    </div>
+  );
+};
+
+export default GoogleAuth;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import GoogleAuth from "./GoogleAuth";
+
+const Header = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default Header;
