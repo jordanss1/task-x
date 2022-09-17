de4f45d8ffe6fcb60454a31e2f6117d59cff42a6
jordanss1 <jordan.shade@live.com>
2022-09-17 13:04:15 +0100

    fixed button not showing up on first render without refreshing the page

--- todo-app-react-redux/src/features/auth/initialiseGoogle.js
+++ todo-app-react-redux/src/features/auth/initialiseGoogle.js
@@ -1,17 +1,19 @@
 export const initialiseGoogle = (callbackResponse) => {
   /* global google */
   const google = window.google;
-  google.accounts.id.initialize({
-    client_id:
-      "600309209777-29sff65hsmud4j0gpt5icis5meaud1de.apps.googleusercontent.com",
-    callback: callbackResponse,
-  });
-  window.google.accounts.id.renderButton(
-    document.getElementById("buttonSignIn"),
-    {
-      shape: "circle",
-      size: "medium",
-      type: "standard",
-    }
-  );
+  if (google) {
+    google.accounts.id.initialize({
+      client_id:
+        "600309209777-29sff65hsmud4j0gpt5icis5meaud1de.apps.googleusercontent.com",
+      callback: callbackResponse,
+    });
+    window.google.accounts.id.renderButton(
+      document.getElementById("buttonSignIn"),
+      {
+        shape: "circle",
+        size: "medium",
+        type: "standard",
+      }
+    );
+  }
 };
