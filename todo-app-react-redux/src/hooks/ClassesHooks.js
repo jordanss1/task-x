117f5f0543899d7eb883666a15765ef535744734
jordanss1 <101638406+jordanss1@users.noreply.github.com>
2023-05-18 16:06:09 +0100

    new function for initial classes

--- todo-app-react-redux/src/hooks/ClassesHooks.js
+++ todo-app-react-redux/src/hooks/ClassesHooks.js
@@ -2,12 +2,28 @@ import { useDispatch } from "react-redux";
 import {
   signInButtonSet,
   signOutButtonSet,
+  initialClassesAdd,
+  initialClassesRemove,
+  loginContainerSet,
 } from "../features/classes/classesSlice";
 
 const useClassesHook = () => {
   const dispatch = useDispatch();
 
-  const setAuthButton = (status) => {
+  const initialClassesFunc = () => {
+    // Classes to be added only on initial rendering
+
+    dispatch(initialClassesAdd());
+    dispatch(loginContainerSet("start-ani"));
+
+    let id = setTimeout(() => {
+      dispatch(initialClassesRemove());
+    }, 3000);
+
+    return id;
+  };
+
+  const setAuthButtonClasses = (status) => {
     const signInButton =
       status === "signingIn" ? "signInButton-out" : "signInButton-in";
     const signOutButton =
@@ -17,7 +33,7 @@ const useClassesHook = () => {
     dispatch(signOutButtonSet(signOutButton));
   };
 
-  return { setAuthButton };
+  return { setAuthButtonClasses, initialClassesFunc };
 };
 
 export default useClassesHook;
