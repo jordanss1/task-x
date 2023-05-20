c508ec08b626867602b99475cb50ef08be9ec5c9
jordanss1 <101638406+jordanss1@users.noreply.github.com>
2023-05-20 20:30:55 +0100

    changed to typescript

---
+++ todo-app-react-redux/src/index.tsx
@@ -0,0 +1,14 @@
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./components/App";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLDivElement
+);
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
