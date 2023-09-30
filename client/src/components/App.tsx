e83b085b0c061644c1fc35389e7ec49d5b833b00
jordanss1 <101638406+jordanss1@users.noreply.github.com>
2023-09-30 14:10:55 +0100

    route added

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,10 +1,15 @@
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { ReactElement } from "react";
+import { Route, Routes } from "react-router-dom";
 import "../styles/all.css";
+import Landing from "./landing/Landing";
 
 const App = (): ReactElement => {
   return (
     <GoogleOAuthProvider clientId={process.env.REACT_APP_ID as string}>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
     </GoogleOAuthProvider>
   );
 };
