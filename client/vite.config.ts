7036e6c25f0b4271c13365eff89c1da6247700e5
jordanss1 <101638406+jordanss1@users.noreply.github.com>
2023-11-27 19:56:05 +0000

    vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ import viteTsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: "build",
+    outDir: "dist",
   },
   server: {
     open: true,
