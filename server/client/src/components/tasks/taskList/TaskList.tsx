ec783012b642c6324ebc7fa54e4a8f739a2998e7
jordanss1 <101638406+jordanss1@users.noreply.github.com>
2023-12-30 22:51:31 +0000

    seeded faker posts added post schema/frontend more url dependant

--- server/client/src/components/tasks/taskList/TaskList.tsx
+++ server/client/src/components/tasks/taskList/TaskList.tsx
@@ -1,11 +1,12 @@
 import dayjs from "dayjs";
 import { ReactElement } from "react";
+import { TaskType } from "../../../types";
 import TaskListTask from "./TaskListTask";
 
-const tasks = [
+const tasks: TaskType[] = [
   {
     task: "Take my last exam of the year for nursing school!",
-    userId: "33",
+    taskId: "454",
     dueDate: dayjs().subtract(2, "week").toDate(),
     enabledDueDate: true,
     created: dayjs().subtract(2, "days").toDate(),
@@ -13,7 +14,7 @@ const tasks = [
   },
   {
     task: "Study for my exam",
-    userId: "33",
+    taskId: "454",
     dueDate: undefined,
     enabledDueDate: false,
     created: dayjs().subtract(5, "days").toDate(),
@@ -21,7 +22,7 @@ const tasks = [
   },
   {
     task: "Take dog for walk",
-    userId: "33",
+    taskId: "454",
     dueDate: dayjs().add(3, "weeks").toDate(),
     enabledDueDate: true,
     created: dayjs().subtract(2, "days").toDate(),
@@ -29,7 +30,7 @@ const tasks = [
   },
   {
     task: "Take my father to his last golf meeting for the next year!",
-    userId: "33",
+    taskId: "454",
     dueDate: dayjs().add(4, "months").toDate(),
     enabledDueDate: true,
     created: dayjs().subtract(5, "days").toDate(),
