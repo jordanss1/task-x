12fcf2d516cc82a7e928f1d9801b96bcc50e8935
jordanss1 <101638406+jordanss1@users.noreply.github.com>
2024-01-04 22:44:17 +0000

    more task routes - pulled tasks to client/redux

--- server/client/src/components/tasks/taskWall/TaskWall.tsx
+++ server/client/src/components/tasks/taskWall/TaskWall.tsx
@@ -1,18 +1,19 @@
 import { ReactElement } from "react";
 import { useSelector } from "react-redux";
 import { taskWallSelector } from "../../../features/taskWall/taskWallSlice";
-import TaskWallNav from "./taskWallNav/TaskWallNav";
 import TaskWallTask from "./TaskWallTask";
+import TaskWallNav from "./taskWallNav/TaskWallNav";
 
 const TaskWall = (): ReactElement => {
-  const { taskWallTasks } = useSelector(taskWallSelector);
+  const { allTaskWallTasks } = useSelector(taskWallSelector);
 
   return (
     <section className="flex max-w-3xl mr-auto ml-auto flex-col gap-8 items-center py-20">
       <TaskWallNav />
-      {taskWallTasks?.map((task, i) => (
-        <TaskWallTask key={i} task={task} />
-      ))}
+      {allTaskWallTasks &&
+        allTaskWallTasks?.map((task, i) => (
+          <TaskWallTask key={i} task={task} />
+        ))}
     </section>
   );
 };
